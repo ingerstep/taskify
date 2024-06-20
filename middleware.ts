3f5680075c2b7c2acf30b93e76866dd0a6ecc37f
Stepan <stepan.karelin.1996@mail.ru>
2024-06-20 19:14:58 +0700

    Connected PostgreSQL

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import {NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']) 
