19860d8bfff96e9478f528ce4d263f92f7635116
Stepan <stepan.karelin.1996@mail.ru>
2024-06-24 08:25:57 +0700

    Created boards list

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,16 @@
+import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
+
+import { Separator } from "@/components/ui/separator";
 
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info/>
+      <Separator className="my-4"/>
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
---
+++ components/form/form-popover.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { FormInput } from "./form-input";
+import { FormSubmit } from "./form-button";
+import { X } from "lucide-react";
+
+import { createBoard } from "@/actions/create-board";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useAction } from "@/hooks/use-action";
+import { Button } from "@/components/ui/button";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+
+export const FormPopover = ({
+  children,
+  align,
+  side = "bottom",
+  sideOffset = 0,
+}: FormPopoverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log({ data });
+    },
+    onError: (error) => {
+      console.log({ error });
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Create board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600 outline-none"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput
+              errors={fieldErrors}
+              id="title"
+              label="Board title"
+              type="text"
+            />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
