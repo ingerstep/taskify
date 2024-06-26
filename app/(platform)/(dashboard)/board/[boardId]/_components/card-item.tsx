8740824695aa8f8bfbc88d83d205ac56c727e5e9
Stepan <stepan.karelin.1996@mail.ru>
2024-06-26 11:54:35 +0700

    Created Card modal

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useCardModal } from "@/hooks/use-card-modal";
 import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 
@@ -9,12 +10,15 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
+  const cardModal = useCardModal();
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
         <div
           {...provided.draggableProps}
           {...provided.dragHandleProps}
+          onClick={() => cardModal.onOpen(data.id)}
           ref={provided.innerRef}
           role="button"
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
